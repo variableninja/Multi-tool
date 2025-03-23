bbbb25dc448e603dc31c881fc5d19041209b4a06
Brunno <145746324+crynew@users.noreply.github.com>
2025-03-23 11:06:08 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -116,11 +116,11 @@ const saveSettings = () => {
   fs.writeFileSync(settingsFilePath, JSON.stringify(settings, null, 2));
 };
 
-const fileUrl = 'https://raw.githubusercontent.com/Victims-Team/Multi-tool/main/src/index.ts';
+const fileUrl = 'https://raw.githubusercontent.com/victims-lol/Multi-tool/main/src/index.ts';
 
 async function checarUpdates(versionAtual: string): Promise<boolean> {
   try {
-    const resposta = await fetch("https://api.github.com/repos/Victims-Team/Multi-tool/releases/latest");
+    const resposta = await fetch("https://api.github.com/repos/victims-lol/Multi-tool/releases/latest");
     const dados = await resposta.json();
     const latestVersion = dados.body;
 
@@ -201,7 +201,6 @@ const showMenu = () => {
   
   if (!process.env.CREATING_EXECUTABLE) {
     console.log(colorful(colors.green, '     [13] Criar Executável.'));
-    console.log(colorful(colors.green, '     [14] Auto Update.'));
   }
   
   console.log(colorful(colors.green, '     [99] Configuracoes.'));
